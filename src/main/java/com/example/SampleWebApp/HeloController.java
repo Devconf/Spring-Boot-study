9fe1d80e70acf75abce49384c913cf0a364a9dd6
kim hyun sik <devconf5296@gmail.com>
2021-05-18 16:51:13 +0900

    폼 사용과 @RequestMapping 수정하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -4,25 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int res = 0;
-		for(int i=0;i<=num;i++) {
-			res+=i;
-		}
-		model.addAttribute("msg","total: "+ res);
-		return "index";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg","이름을 적어서 전송해주세요.");
+		mav.setViewName("index");
+		return mav;
 	}
 	
-	@RequestMapping("/main/{name}")
-	public ModelAndView mainPage(@PathVariable String name, ModelAndView mav) {
-		mav.addObject("name",name);
-		mav.setViewName("main");
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1") String str,ModelAndView mav) {
+		mav.addObject("msg","안녕하세요! "+ str +"님!");
+		mav.setViewName("index");
 		return mav;
 	}
 }
