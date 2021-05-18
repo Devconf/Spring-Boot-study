e4fb3c525f997eaf589991addda7715ab829e09f
kim hyun sik <devconf5296@gmail.com>
2021-05-18 14:00:13 +0900

    @Controller와 thymeleaf(view template)를 이용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -1,58 +1,14 @@
 package com.example.SampleWebApp;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController //json형식으로 반환하는 컨트롤러
+
+@Controller //spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 	
-	String[] names = {"Kim","lee","park","choi","jo"};
-	String[] mails = {"kin@tuuyano.com",
-			"lee@flower","park@yamda",
-			"choi@happy","jo@baseball"};
-	
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-		DataObject object = new DataObject(id,names[id],mails[id]); 
-		System.out.println(object.getClass().getName());
-		return object;
-	}
-	
-	class DataObject{
-		private int id;
-		private String name;
-		private String value;
-		
-		public DataObject(int id, String name, String value) {
-			super();
-			this.id= id;
-			this.name=name;
-			this.value=value;
-		}
-		
-		public int getId() {
-			return this.id;
-		}
-		
-		public void setId(int id) {
-			this.id=id;
-		}
-		
-		public String getName() {
-			return this.name;
-		}
-		
-		public void setName(String name) {
-			this.name=name;
-		}
-		
-		public String getValue() {
-			return this.value;
-		}
-		
-		public void setValue(String value) {
-			this.value=value;
-		}
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
 }
