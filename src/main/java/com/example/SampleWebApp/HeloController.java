75ea3956772c873ae8231ed228e98d49d9d87776
kim hyun sik <devconf5296@gmail.com>
2021-05-18 16:53:29 +0900

    @RequestMapping 변형하기 @GetMapping, @PostMapping 사용

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -1,24 +1,22 @@
 package com.example.SampleWebApp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.addObject("msg","이름을 적어서 전송해주세요.");
 		mav.setViewName("index");
 		return mav;
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@PostMapping("/")
 	public ModelAndView send(@RequestParam("text1") String str,ModelAndView mav) {
 		mav.addObject("msg","안녕하세요! "+ str +"님!");
 		mav.setViewName("index");
