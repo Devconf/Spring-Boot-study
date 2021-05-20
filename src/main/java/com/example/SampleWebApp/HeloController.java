416acb4355aa7496d3cacc97929b10e0906d49d5
kim hyun sik <devconf5296@gmail.com>
2021-05-20 17:52:01 +0900

    기타 폼 사용하기 @RequestParam 심화

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -11,14 +11,34 @@ public class HeloController {
 
 	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","이름을 적어서 전송해주세요.");
+		mav.addObject("msg","폼을 전송해주세요.");
 		mav.setViewName("index");
 		return mav;
 	}
 	
 	@PostMapping("/")
-	public ModelAndView send(@RequestParam("text1") String str,ModelAndView mav) {
-		mav.addObject("msg","안녕하세요! "+ str +"님!");
+	public ModelAndView send(
+			@RequestParam(value = "check1", required = false) boolean check1,
+			@RequestParam(value = "radio1", required = false) String radio1,
+			@RequestParam(value = "select1", required = false) String select1,
+			@RequestParam(value = "select2", required = false) String[] select2,
+			ModelAndView mav) {
+		
+		String res="";
+		try {
+			res = "check1 : "+ check1+ " radio1 : "+ radio1 +
+					" select1 : " + select1 + "\nselect2 : ";
+		}catch (Exception e) {}
+		try {
+			res += select2[0];
+			for(int i=1;i<select2.length;i++) {
+				res+= ", "+ select2[i];
+			}
+			
+		} catch (Exception e) {
+			res += null;
+		}
+		mav.addObject("msg",res);
 		mav.setViewName("index");
 		return mav;
 	}
