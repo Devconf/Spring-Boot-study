155e8b2a6a03fc746964d4c5372a081af8bfa016
kim hyun sik <devconf5296@gmail.com>
2021-05-22 17:31:13 +0900

    리다이랙션 활용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -11,35 +11,54 @@ public class HeloController {
 
 	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","폼을 전송해주세요.");
+		mav.addObject("msg", "폼을 전송해주세요.");
 		mav.setViewName("index");
 		return mav;
 	}
-	
+
 	@PostMapping("/")
-	public ModelAndView send(
-			@RequestParam(value = "check1", required = false) boolean check1,
+	public ModelAndView send(@RequestParam(value = "check1", required = false) boolean check1,
 			@RequestParam(value = "radio1", required = false) String radio1,
 			@RequestParam(value = "select1", required = false) String select1,
-			@RequestParam(value = "select2", required = false) String[] select2,
-			ModelAndView mav) {
-		
-		String res="";
+			@RequestParam(value = "select2", required = false) String[] select2, ModelAndView mav) {
+
+		String res = "";
 		try {
-			res = "check1 : "+ check1+ " radio1 : "+ radio1 +
-					" select1 : " + select1 + "\nselect2 : ";
-		}catch (Exception e) {}
+			res = "check1 : " + check1 + " radio1 : " + radio1 + " select1 : " + select1 + "\nselect2 : ";
+		} catch (Exception e) {
+		}
 		try {
 			res += select2[0];
-			for(int i=1;i<select2.length;i++) {
-				res+= ", "+ select2[i];
+			for (int i = 1; i < select2.length; i++) {
+				res += ", " + select2[i];
 			}
-			
+
 		} catch (Exception e) {
 			res += null;
 		}
-		mav.addObject("msg",res);
+		mav.addObject("msg", res);
 		mav.setViewName("index");
 		return mav;
 	}
+
+	
+	@PostMapping(value = "/main", name = "signIn") 
+	public ModelAndView signIn(
+			@RequestParam(value = "userID", required = true) String userId,
+			@RequestParam(value = "userPW",required = true) String userPW,
+			ModelAndView mav) {
+		if( userId.equals("devconf") && userPW.equals("132600qa@")) {
+			mav.addObject("name", "hyunsik");
+			System.out.println("success");
+			}
+		mav.setViewName("main");
+		return mav; 
+	}		 
+	
+	@GetMapping(value = "/main", name = "signOut") 
+	public ModelAndView signOut(
+			ModelAndView mav) {
+		mav.setViewName("redirect:/");
+		return mav; 
+	}		 
 }
