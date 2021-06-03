2b6f100e334aa943b05e17e65386e72c705d176b
kim hyun sik <devconf5296@gmail.com>
2021-06-03 15:03:31 +0900

    Thymeleaf 삼항연산자 사용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -2,15 +2,19 @@ package com.example.SampleWebApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@GetMapping("/")
-	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg", "message 1<hr/>message 2<br/>message 3");
+	@GetMapping("/{id}")
+	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("id",id);
+		mav.addObject("check",id%2==0);
+		mav.addObject("trueValue","Even number!");
+		mav.addObject("falseValue","Odd number...");
 		return mav; 
 	}
 }
