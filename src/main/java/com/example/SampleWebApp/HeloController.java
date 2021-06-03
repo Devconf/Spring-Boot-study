0ba3fd0a3cc0e6105702ad58fb8c0a268a85d88d
kim hyun sik <devconf5296@gmail.com>
2021-06-03 15:32:02 +0900

    Thymeleaf th:switch, th:case 활용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@GetMapping("/{id}")
-	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
+	@GetMapping("/{month}")
+	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("id",id);
-		mav.addObject("check",id>=0);
-		mav.addObject("trueValue","POSITIVE!");
-		mav.addObject("falseValue","negative...");
+		int m= Math.abs(month)%12;
+		m = m==0 ? 12 : m;
+		mav.addObject("month",m);
+		mav.addObject("check",Math.floor(m/3));
 		return mav; 
 	}
 }
