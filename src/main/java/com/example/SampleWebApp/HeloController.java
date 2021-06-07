8891c6fdb2f91747f62845732a6537b951bae62b
kim hyun sik <devconf5296@gmail.com>
2021-06-07 17:57:47 +0900

    Thymeleaf 전처리 활용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -10,15 +10,59 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@GetMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@GetMapping("/{num}")
+	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
 		mav.setViewName("index");
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		data.add(new String[] {"park","park@yamada","090-999-999"});
-		data.add(new String[] {"lee","lee@flower","080-888-888"});
-		data.add(new String[] {"choi","choi@happy","080-888-888"});
+		mav.addObject("num",num);
+		if(num>=0) {
+			mav.addObject("check","num>=data.size() ? 0 : num");
+		}
+		else {
+			mav.addObject("check","num<=data.size()*-1 ? 0 : num*-1");
+		}
+		ArrayList<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0, "park","park@yamada"));
+		data.add(new DataObject(1, "lee", "lee@flower"));
+		data.add(new DataObject(2, "choi","choi@happy"));
 		mav.addObject("data",data);
 		return mav; 
+	}
+	
+	class DataObject{
+		private int id;
+		private String name;
+		private String value;
+		
+		public DataObject(int id, String name, String value) {
+			super();
+			this.id= id;
+			this.name=name;
+			this.value=value;
+		}
+		
+		public int getId() {
+			return this.id;
+		}
+		
+		public void setId(int id) {
+			this.id=id;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public void setName(String name) {
+			this.name=name;
+		}
+		
+		public String getValue() {
+			return this.value;
+		}
+		
+		public void setValue(String value) {
+			this.value=value;
+		}
 	}
 }
 
