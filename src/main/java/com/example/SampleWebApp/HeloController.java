8d32fb72a8a400c01955f8d994b1deb315826936
kim hyun sik <devconf5296@gmail.com>
2021-06-07 16:47:50 +0900

    Thymeleaf th:each 활용하기

--- src/main/java/com/example/SampleWebApp/HeloController.java
+++ src/main/java/com/example/SampleWebApp/HeloController.java
@@ -1,5 +1,7 @@
 package com.example.SampleWebApp;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,13 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller // spring-boot-starter-thymeleaf dependencies를 설정 해주었기 때문에 view탬플릿으로 동작 가능
 public class HeloController {
 
-	@GetMapping("/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+	@GetMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		int m= Math.abs(month)%12;
-		m = m==0 ? 12 : m;
-		mav.addObject("month",m);
-		mav.addObject("check",Math.floor(m/3));
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"park","park@yamada","090-999-999"});
+		data.add(new String[] {"lee","lee@flower","080-888-888"});
+		data.add(new String[] {"choi","choi@happy","080-888-888"});
+		mav.addObject("data",data);
 		return mav; 
 	}
 }
