003216da36506f2b7387c47078af8186a4073717
kim hyun sik <devconf5296@gmail.com>
2021-06-11 19:47:08 +0900

    JpaRepository 메서드 구현하기

--- src/main/java/com/example/SampleWebApp/repository/MyDataRepository.java
+++ src/main/java/com/example/SampleWebApp/repository/MyDataRepository.java
@@ -1,12 +1,18 @@
 package com.example.SampleWebApp.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.example.SampleWebApp.MyData;
 
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long> {
-	Optional<MyData> findById(Long name);	
+	Optional<MyData> findById(Long name);
+	public List<MyData> findByNameLike(String name);
+	public List<MyData> findByIdIsNotNullOrderByIdDesc();
+	public List<MyData> findByAgeGreaterThan(Integer age);
+	public List<MyData> findByAgeBetween(Integer age1, Integer age2);
 }
