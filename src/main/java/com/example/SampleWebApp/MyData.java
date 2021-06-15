57f143ed130d376f84619c79f30ad8f7eec91961
kim hyun sik <devconf5296@gmail.com>
2021-06-15 17:24:16 +0900

    Entity에 오류 메시지 작성하기

--- src/main/java/com/example/SampleWebApp/MyData.java
+++ src/main/java/com/example/SampleWebApp/MyData.java
@@ -22,16 +22,16 @@ public class MyData {
 	private long id;
 	
 	@Column(length = 50, nullable = false)
-	@NotEmpty
+	@NotEmpty(message = "공백 불가")
 	private String name;
 	
 	@Column(length = 200, nullable = true)
-	@Email
+	@Email(message = "메일 주소만")
 	private String mail;
 	
 	@Column(nullable = true)
-	@Min(0)
-	@Max(200)
+	@Min(value = 0, message = "0 이상")
+	@Max(value= 200, message = "200 이하")
 	private int age;
 	
 	@Column(nullable = true)
