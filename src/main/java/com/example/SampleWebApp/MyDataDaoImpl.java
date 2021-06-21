453f1d4237e93323edbf1ce428d56c98a66a1d7f
kim hyun sik <devconf5296@gmail.com>
2021-06-21 18:45:28 +0900

    @NamedQuery& @NamedQueries를 이용한 복수 매개변수와 메서드 체인

--- src/main/java/com/example/SampleWebApp/MyDataDaoImpl.java
+++ src/main/java/com/example/SampleWebApp/MyDataDaoImpl.java
@@ -43,14 +43,13 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 	@Override
 	public List<MyData> find(String fstr) {
 		List<MyData> list =null;
-		String qstr ="from MyData where id = :fid or name like :fname or mail like :fmail";
 		Long fid =0L;
 		try {
 			fid= Long.parseLong(fstr);
 		}catch(NumberFormatException e) {
 			//e.printStackTrace();
 		}
-		Query query = entityManager.createQuery(qstr).setParameter("fid", fid)
+		Query query = entityManager.createNamedQuery("findWithIdAndNameAndMail").setParameter("fid", fid)
 				.setParameter("fname", "%" + fstr + "%")
 				.setParameter("fmail", fstr + "@%");
 		list =query.getResultList();
